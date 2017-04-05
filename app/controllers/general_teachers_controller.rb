857749fbb5af94e4a6becaea203ec379ae15b0bb
mikeposner <cyhm_jjd@163.com>
2017-04-05 17:15:34 +0800

    手动输入空格尝试解决teacher控制器的缩进问题

--- app/controllers/general_teachers_controller.rb
+++ app/controllers/general_teachers_controller.rb
@@ -1,45 +1,44 @@
 class GeneralTeachersController < ApplicationController
 
-	def new 
-		@user=User.new
-	end
+  def new 
+    @user=User.new
+  end
 
-	def show
-		@user = User.find session[:user]['id']
-		@student=User.where category:3
-		@student_name=""
-		@student.size>=1?@student_name=@student.map(&:name).join('、'):@student_name=@student.name
-	end
+  def show
+    @user = User.find session[:user]['id']
+    @student=User.where category:3
+    @student_name=""
+    @student.size>=1?@student_name=@student.map(&:name).join('、'):@student_name=@student.name
+  end
 
-	def edit
-		@user = User.find(session[:user]['id'])
-	end
+  def edit
+    @user = User.find(session[:user]['id'])
+  end
 
-	def update
-		@user = User.find(session[:user]['id'])
-		@room = Room.where(name:@user.rooms.first.name)
-		@room.update(room_params[:rooms])
-		if @user.update(user_params)
-			session[:user] = @user
-			redirect_to :general_teachers_show
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @user = User.find(session[:user]['id'])
+    @room = Room.where(name:@user.rooms.first.name)
+    @room.update(room_params[:rooms])
+    if @user.update(user_params)
+      session[:user] = @user
+      redirect_to :general_teachers_show
+    else
+      render 'edit'
+    end
+  end
 
-	private
+  private
 
-		def user_params
-			params.require(:user).permit(:name,:category,:password)
-			# params.permit(user:[:name,{rooms: :room_name},:category,:password])
-		end
+    def user_params
+      params.require(:user).permit(:name,:category,:password)
+      # params.permit(user:[:name,{rooms: :room_name},:category,:password])
+    end
 
-		def room_params
-			# params.permit(user:[rooms: :room_name])
-			params.require(:user).permit(rooms: :name)
-		end
-
-	end
+    def room_params
+      # params.permit(user:[rooms: :room_name])
+      params.require(:user).permit(rooms: :name)
+    end
+  end
 
 
 
