6c2abdce9772ca8cf037b3d20e51fcb16b152ade
mikeposner <cyhm_jjd@163.com>
2017-04-05 17:25:09 +0800

    尝试使用atom的tab键解决模型user的缩进问题

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	has_secure_password
-	has_many :user_rooms
-	has_many :rooms, :through => :user_rooms
+  has_secure_password
+  has_many :user_rooms
+  has_many :rooms, :through => :user_rooms
 end
